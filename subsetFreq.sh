96f054420d5717152459f2a09fb8a4b6b7544cf2
em255 <eml255@cornell.edu>
2024-06-12 13:42:25 -0700

    Update Freq subset to add header with Multiple empty columns to account for multiallelic sites

--- subsetFreq.sh
+++ subsetFreq.sh
@@ -1,1 +1,4 @@
-grep $1 $2 > Freq/${1}.frq
+head -n 1 $2 | awk -v OFS="\t" '{print $1,$2,$3,NA,NA,NA,NA,NA,NA}' > Freq/${1}.frq
+grep $1 $2 > Freq/${1}.frq2
+cat Freq/${1}.frq2 >> Freq/${1}.frq
+rm Freq/${1}.frq2
